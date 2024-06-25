539573d3d421860212d125c47c22a8fd1fa6416d
Nam Vũ <69331696+borealis2311@users.noreply.github.com>
2024-06-25 11:09:16 +0700

    First commit

--- src/apps/middlewares/admin/auth.js
+++ src/apps/middlewares/admin/auth.js
@@ -1,13 +1,13 @@
 exports.notLoggedAdmin = (req, res, next)=>{
-    // if(!req.session.email){
-    //     res.redirect("/admin/login")
-    // };
+    if(!req.session.email){
+        res.redirect("/admin/login")
+    };
     next();
 }
 
 exports.loggedAdmin = (req, res, next)=>{
-    // if(req.session.email){
-    //     res.redirect("/admin/dashboard")
-    // };
+    if(req.session.email){
+        res.redirect("/admin/dashboard")
+    };
     next();
 }
